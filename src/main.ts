261935903882109455ff99c20ce6757819e7261c
alvinalzali <alvinamaterasu@gmail.com>
2026-07-03 09:49:29 +0700

    edited git docker

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,16 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }));
 
   app.enableCors();
 
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
+  
+  console.log(`Application is running on: http://0.0.0.0:${port}`);
 }
 bootstrap();
